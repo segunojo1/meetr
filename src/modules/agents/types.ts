abb456df343e188ee9105bde8ed6e6ae2e728fc0
Segun Ojo <olusegunkd2017@gmail.com>
2026-05-01 04:19:30 +0100

    fix getmany

--- src/modules/agents/types.ts
+++ src/modules/agents/types.ts
@@ -3,4 +3,4 @@ import type {AppRouter} from "@/trpc/routers/_app";
 import { inferRouterOutputs } from "@trpc/server";
 
 export type AgentGetOne = inferRouterOutputs<AppRouter>["agents"]["getOne"];
-export type AgentGetMany = inferRouterOutputs<AppRouter>["agents"]["getMany"];
+export type AgentsGetMany = inferRouterOutputs<AppRouter>["agents"]["getMany"]["items"];
