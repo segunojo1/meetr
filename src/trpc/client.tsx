b617c80de21fd67a1ff6ff4ba9aed92ad02a2675
Segun Ojo <olusegunkd2017@gmail.com>
2026-05-01 04:52:30 +0100

    change base url

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -29,7 +29,7 @@ function getUrl() {
   const base = (() => {
     if (typeof window !== 'undefined') return '';
     if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-    return 'http://localhost:3000';
+    return 'https://meetr-xi.vercel.app';
   })();
   return `${base}/api/trpc`;
 }
